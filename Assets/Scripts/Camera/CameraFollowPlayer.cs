ab598a3c87348e69fed51e0399985987f76b4bc5
bhanyagici <bhanyagicigmail.com>
2023-06-07 21:13:26 +0300

    Added Player Controller and Camera Follow

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed = 10.0f;
+    public Vector3 distance;
+    public Transform lookTarget;
+    // Start is called before the first frame update
+    private void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + distance;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget);
+    }
+
+    private void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
