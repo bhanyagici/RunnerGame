5c9b1bdd9af14d1619d101cdf7bbbbafa62abf0a
bhanyagici <bhanyagicigmail.com>
2023-07-27 02:02:37 +0300

    Add materials, Object parents, Collision  and Collider control, restart scene

--- Assets/Scripts/Player/CollectCoin.cs
+++ Assets/Scripts/Player/CollectCoin.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollectCoin : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI CoinText;
+    PlayerController playerController;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             AddCoin();
             Destroy(other.gameObject);
-            //Debug.Log(other.gameObject.name);
+        }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("Congrats!...");
+            playerController.runningSpeed = 0;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Touched Obstacle!..");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
