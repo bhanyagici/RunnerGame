bbaa22aeb649550a046ac9c4b1c2f7941c787090
bhanyagici <bhanyagicigmail.com>
2023-07-27 00:54:25 +0300

    Coin prefab remake and make obstacle

--- Assets/Scripts/Player/CollectCoin.cs
+++ Assets/Scripts/Player/CollectCoin.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectCoin : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI CoinText;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
+            AddCoin();
             Destroy(other.gameObject);
-            Debug.Log(other.gameObject.name);
+            //Debug.Log(other.gameObject.name);
         }
+    }
+
+    public void AddCoin()
+    {
+        score++;
+        CoinText.text = "Score: " + score.ToString();
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,11 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         SwipeCheck();
-
-
-
     }
 
     private void SwipeCheck()
