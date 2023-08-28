7941176f59edf00d9c2cb453a447260fd20c8454
bhanyagici <bhanyagicigmail.com>
2023-08-28 23:11:29 +0300

    commit
    Add Skybox, win-lose animation and gameover panel

--- Assets/Scripts/Player/CollectCoin.cs
+++ Assets/Scripts/Player/CollectCoin.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro; //TextMeshPro'ya ulaþabilmemizi saðlýyor.
 using UnityEngine.SceneManagement;// Sahnelere ulaþabilmemizi saðlýyor.
+using UnityEditor.Experimental.GraphView;
 
 public class CollectCoin : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI CoinText;
-    PlayerController playerController;
+    public PlayerController playerController;
+    public int maxScore;
+    public Animator PlayerAnim;
+    public GameObject Player;
+    public GameObject EndPanel;
 
-
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.CompareTag("Coin"))
-        {
-            AddCoin();
-            Destroy(other.gameObject);
-        }
-        else if (other.CompareTag("End"))
-        {
-            Debug.Log("Congrats!...");
-            playerController.runningSpeed = 0;
-
-        }
+        PlayerAnim = Player.GetComponentInChildren<Animator>();
     }
+
+
+
+       private void OnTriggerEnter(Collider other)
+     {
+         if (other.CompareTag("Coin"))
+         {
+             AddCoin();
+             Destroy(other.gameObject);
+         }
+         /*else if (other.CompareTag("End"))
+         {
+             playerController.runningSpeed = 0;
+
+            if (score >= maxScore)
+            {
+                Debug.Log("You Win!..");
+            }
+            else
+            {
+                Debug.Log("You Lose!..");
+            }
+         }*/
+     }
+
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -34,7 +53,30 @@ public class CollectCoin : MonoBehaviour
             //Sahnenin yemiden baþlatýlmasýný saðlýyor.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        else if(collision.gameObject.CompareTag("End"))
+        {
+            playerController.runningSpeed = 0;
+
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+
+            EndPanel.SetActive(true);
+
+            if (score >= maxScore)
+            {
+                PlayerAnim.SetBool("win", true);
+            }
+            else
+            {
+                PlayerAnim.SetBool("lose", true);
+            }
+        }
     }
+
+    public void RestarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 
     public void AddCoin()
     {
